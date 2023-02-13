459ccea53f04841a7e79d4f23f68c5feabc62cd1
raeoxfordhebron <raeoxfordhebron@gmail.com>
2023-02-13 16:09:28 -0500

    add notes index route

--- controllers/note.js
+++ controllers/note.js
@@ -3,11 +3,14 @@ import Note from "../models/notes.js"
 import isLoggedIn from '../utils/isLoggedIn.js'
 
 const router = express.Router()
+router.use(isLoggedIn)
 
 // Index Route
 router.get("/", async (req, res)=> {
     try{ 
-
+        const username = req.payload.username
+        const notes = await Note.find(username)
+        res.json(notes)
     } catch(error) {
         res.status(400).json(error)
     }
